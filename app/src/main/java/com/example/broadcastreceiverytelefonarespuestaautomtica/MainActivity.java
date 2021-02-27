85da9ae286113442918d6590aca65d572973ea30
Yolanda Zavala <s16120276@alumnos.itsur.edu.mx>
2021-02-27 11:52:46 -0600

    Mensaje y numero obtenidos desde archivo

--- app/src/main/java/com/example/broadcastreceiverytelefonarespuestaautomtica/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiverytelefonarespuestaautomtica/MainActivity.java
@@ -18,19 +18,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtMensaje=(EditText)findViewById(R.id.txtMensaje);
-        button=(Button)findViewById(R.id.button);
+        txtMensaje=findViewById(R.id.txtMensaje);
+        txtNumero=findViewById(R.id.txtNumero);
+        button=findViewById(R.id.button);
         _ctx = this;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //ENVIAMOS UN BROADCAST CON EL MENSAJE DE LA CAJA DE TEXTO QUE INGRESO EL USUARIO PARA GUARDARLO EN LA PROXIMA LLAMADA QUE ENTRE.
-                Intent intent= new Intent("my.action.string");
-                Toast.makeText(_ctx,txtMensaje.getText().toString(),Toast.LENGTH_LONG).show();
-                intent.putExtra("Mensaje",txtMensaje.getText().toString());
-                sendBroadcast(intent);
+                try{
+                    //Escribir datos en el archivo
+                    String cad =txtNumero.getText().toString()+","+txtMensaje.getText().toString();
+                    Archivo.writeToFile(cad,getApplicationContext());
+                    //Intent intent= new Intent("my.action.string");
+                    Intent intent = new Intent();
+                    intent.setAction("com.example.broadcastreceiverytelefonarespuestaautomtica");
+                    //intent.putExtra("Numero",txtNumero.getText().toString());
+                    //intent.putExtra("Mensaje",txtMensaje.getText().toString()+"");
+                    sendBroadcast(intent);
+                    Toast.makeText(_ctx,"Guardado"+cad,Toast.LENGTH_LONG).show();
+                }catch(Exception e){
+                    Toast.makeText(_ctx,e+"",Toast.LENGTH_LONG).show();
+                }
             }
         });
-
     }
 }
--- app/src/main/java/com/example/broadcastreceiverytelefonarespuestaautomtica/llamadaentrante.java
+++ app/src/main/java/com/example/broadcastreceiverytelefonarespuestaautomtica/llamadaentrante.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -18,52 +19,49 @@ public class llamadaentrante extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         //OBTENEMOS EL MENSAJE ENVIADO DE LA CAJA DE TEXTO PARA GUARDARLO EN UNA VARIABLE
-        String mensaje = intent.getStringExtra("Mensaje");
-        if (mensaje != null) {
-            mensajeGuardado = mensaje;
+        String[] arch = Archivo.readFromFile(context).split(",");
+        mensajeGuardado=arch[1];
+        if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+            incomingFlag = false;
         } else {
-            Toast.makeText(context, "desde el receive", Toast.LENGTH_LONG).show();
-            if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
-                Toast.makeText(context, "else if", Toast.LENGTH_LONG).show();
-                incomingFlag = false;
-                String phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-            } else {
-                Toast.makeText(context, "else else", Toast.LENGTH_LONG).show();
-                // TELEPHONY MANAGER OBTENIENDO EL SERVICIO DE TELEFONIA.
-                TelephonyManager tm = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
-                //SWITCH QUE PREGUNTA POR EL ESTADO DE MANAGER SI SE ENCUENTRA EN RINGING(SONANDO) O IDLE.
-                switch (tm.getCallState()) {
-                    // SI EL ESTADO DEL MANAGER ES UN RINGING ES PORQUE HAY UNA LLAMADA ENTRANTE O SONANDO, ENTONCES CAPTURAMOS EL NUMERO TELEFONICO.
-                    case TelephonyManager.CALL_STATE_RINGING:
-                        incomingFlag = true;
-                        //incoming_number = intent.getStringExtra("incoming_number"); //OBTENEMOS EL NUMERO DESDE EL INTENT.
-                        incoming_number = "4451943656";
-                        Toast.makeText(context, "RINGING :" + incoming_number, Toast.LENGTH_LONG).show();
+            // TELEPHONY MANAGER OBTENIENDO EL SERVICIO DE TELEFONIA.
+            TelephonyManager tm = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
+            //SWITCH QUE PREGUNTA POR EL ESTADO DE MANAGER SI SE ENCUENTRA EN RINGING(SONANDO) O IDLE.
+            switch (tm.getCallState()) {
+                // SI EL ESTADO DEL MANAGER ES UN RINGING ES PORQUE HAY UNA LLAMADA ENTRANTE O SONANDO, ENTONCES CAPTURAMOS EL NUMERO TELEFONICO.
+                case TelephonyManager.CALL_STATE_RINGING:
+                    Toast.makeText(context, "Ringing", Toast.LENGTH_LONG).show();
+                    incomingFlag = true;
+                    incoming_number = arch[0];
+                    //Si el incoming_number es igual al que esta sonando
+                    if(incoming_number.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
                         try{
-                            SmsManager smgr = SmsManager.getDefault(); //CREANDO UN OBJETO DE SMS MANAGER.
-                            smgr.sendTextMessage(incoming_number, null, "Qui hubo? uwu", null, null); // ENVIAMOS EL SMS CON EL MENSAJE GUARDADO Y EL NUMERO TELEFONICO.
+                            //CREANDO UN OBJETO DE SMS MANAGER.
+                            SmsManager smgr = SmsManager.getDefault();
+                            // ENVIAMOS EL SMS CON EL MENSAJE GUARDADO Y EL NUMERO TELEFONICO.
+                            smgr.sendTextMessage(incoming_number, null, mensajeGuardado, null, null);
                             Toast.makeText(context, "Enviando mensaje a :" + incoming_number + " " + mensajeGuardado, Toast.LENGTH_LONG).show();
-                            Toast.makeText(context,"Ringing", Toast.LENGTH_LONG).show();
                         }catch (Exception e){
                             Toast.makeText(context,"Error: "+e, Toast.LENGTH_LONG).show();
                         }
-
-                        break;
+                    }else{
+                        Toast.makeText(context, incoming_number+"!="+TelephonyManager.EXTRA_STATE_RINGING, Toast.LENGTH_LONG).show();
+                    }
+                    break;
                     // SI CONTESTAS LA LLAMADA.
-                    case TelephonyManager.CALL_STATE_OFFHOOK:
-                        if (incomingFlag) {
-                            Toast.makeText(context, "contesto e incomming accept", Toast.LENGTH_LONG).show();
-                            Log.i(TAG, "incoming ACCEPT :" + incoming_number);
-                        }
-                        break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    if (incomingFlag) {
+                        Log.i(TAG, "incoming ACCEPT :" + incoming_number);
+                    }
+                    break;
                     //AL TERMINAR LA LLAMADA
-                    case TelephonyManager.CALL_STATE_IDLE:
-                        if (incomingFlag) {
-                            Toast.makeText(context, "incoming IDLE", Toast.LENGTH_LONG).show();
-                        }
-                        break;
+                case TelephonyManager.CALL_STATE_IDLE:
+                    if (incomingFlag) {
+                        Toast.makeText(context, "incoming IDLE", Toast.LENGTH_LONG).show();
+                    }
+                    break;
                 }
             }
         }
     }
-}
+
