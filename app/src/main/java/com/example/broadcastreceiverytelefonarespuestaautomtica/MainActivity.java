c978b3edcfd22cc78fe16cc560a44aa7f05387ca
Yolanda Zavala <s16120276@alumnos.itsur.edu.mx>
2021-02-28 20:57:15 -0600

    Terminado

--- app/src/main/java/com/example/broadcastreceiverytelefonarespuestaautomtica/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiverytelefonarespuestaautomtica/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.broadcastreceiverytelefonarespuestaautomtica;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +16,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText txtMensaje, txtNumero;
-    Context _ctx;
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +24,27 @@ public class MainActivity extends AppCompatActivity {
         txtMensaje=findViewById(R.id.txtMensaje);
         txtNumero=findViewById(R.id.txtNumero);
         button=findViewById(R.id.button);
-        _ctx = this;
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CALL_LOG)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.SEND_SMS,
+                            Manifest.permission.READ_CALL_LOG}, 1);
+        }
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //ENVIAMOS UN BROADCAST CON EL MENSAJE DE LA CAJA DE TEXTO QUE INGRESO EL USUARIO PARA GUARDARLO EN LA PROXIMA LLAMADA QUE ENTRE.
                 try{
                     //Escribir datos en el archivo
                     String cad =txtNumero.getText().toString()+","+txtMensaje.getText().toString();
                     Archivo.writeToFile(cad,getApplicationContext());
-                    //Intent intent= new Intent("my.action.string");
-                    Intent intent = new Intent();
-                    intent.setAction("com.example.broadcastreceiverytelefonarespuestaautomtica");
-                    //intent.putExtra("Numero",txtNumero.getText().toString());
-                    //intent.putExtra("Mensaje",txtMensaje.getText().toString()+"");
-                    sendBroadcast(intent);
-                    Toast.makeText(_ctx,"Guardado"+cad,Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this,"Guardado"+cad,Toast.LENGTH_LONG).show();
                 }catch(Exception e){
-                    Toast.makeText(_ctx,e+"",Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this,e+"",Toast.LENGTH_LONG).show();
                 }
             }
         });
--- app/src/main/java/com/example/broadcastreceiverytelefonarespuestaautomtica/llamadaentrante.java
+++ app/src/main/java/com/example/broadcastreceiverytelefonarespuestaautomtica/llamadaentrante.java
@@ -1,67 +1,67 @@
 package com.example.broadcastreceiverytelefonarespuestaautomtica;
 
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
 public class llamadaentrante extends BroadcastReceiver {
-    private static final String TAG = "PhoneStatReceiver";
-    private static boolean incomingFlag = false;
-    private static String incoming_number = null;
-    private static String mensajeGuardado = null;
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        //OBTENEMOS EL MENSAJE ENVIADO DE LA CAJA DE TEXTO PARA GUARDARLO EN UNA VARIABLE
-        String[] arch = Archivo.readFromFile(context).split(",");
-        mensajeGuardado=arch[1];
-        if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
-            incomingFlag = false;
-        } else {
-            // TELEPHONY MANAGER OBTENIENDO EL SERVICIO DE TELEFONIA.
-            TelephonyManager tm = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
-            //SWITCH QUE PREGUNTA POR EL ESTADO DE MANAGER SI SE ENCUENTRA EN RINGING(SONANDO) O IDLE.
-            switch (tm.getCallState()) {
-                // SI EL ESTADO DEL MANAGER ES UN RINGING ES PORQUE HAY UNA LLAMADA ENTRANTE O SONANDO, ENTONCES CAPTURAMOS EL NUMERO TELEFONICO.
-                case TelephonyManager.CALL_STATE_RINGING:
-                    Toast.makeText(context, "Ringing", Toast.LENGTH_LONG).show();
-                    incomingFlag = true;
-                    incoming_number = arch[0];
-                    //Si el incoming_number es igual al que esta sonando
-                    if(incoming_number.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-                        try{
-                            //CREANDO UN OBJETO DE SMS MANAGER.
-                            SmsManager smgr = SmsManager.getDefault();
-                            // ENVIAMOS EL SMS CON EL MENSAJE GUARDADO Y EL NUMERO TELEFONICO.
-                            smgr.sendTextMessage(incoming_number, null, mensajeGuardado, null, null);
-                            Toast.makeText(context, "Enviando mensaje a :" + incoming_number + " " + mensajeGuardado, Toast.LENGTH_LONG).show();
-                        }catch (Exception e){
-                            Toast.makeText(context,"Error: "+e, Toast.LENGTH_LONG).show();
-                        }
-                    }else{
-                        Toast.makeText(context, incoming_number+"!="+TelephonyManager.EXTRA_STATE_RINGING, Toast.LENGTH_LONG).show();
-                    }
-                    break;
-                    // SI CONTESTAS LA LLAMADA.
-                case TelephonyManager.CALL_STATE_OFFHOOK:
-                    if (incomingFlag) {
-                        Log.i(TAG, "incoming ACCEPT :" + incoming_number);
-                    }
-                    break;
-                    //AL TERMINAR LA LLAMADA
-                case TelephonyManager.CALL_STATE_IDLE:
-                    if (incomingFlag) {
-                        Toast.makeText(context, "incoming IDLE", Toast.LENGTH_LONG).show();
-                    }
-                    break;
-                }
+        TelephonyManager mTelManager= (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        mTelManager.listen(new TeleListener(context),PhoneStateListener.LISTEN_CALL_STATE);
+        }
+    }
+class TeleListener extends PhoneStateListener{
+    private Context ctx;
+    public  TeleListener (Context ctx){this.ctx=ctx;}
+    @Override
+    public void onCallStateChanged(int state, String phoneNumber) {
+        super.onCallStateChanged(state, phoneNumber);
+        String[] arch =Archivo.readFromFile(ctx).split(",");
+        String numero=limpiarNumero(arch[0]);
+        String mensaje=arch[1];
+        if(TelephonyManager.CALL_STATE_RINGING==state){
+            Log.d("IDEA",phoneNumber);
+            phoneNumber=limpiarNumero(phoneNumber);
+            if(phoneNumber.equals(numero)){
+                sendSMS(numero,mensaje,ctx);
+            }else{
+                Toast.makeText(ctx,numero+ " != "+phoneNumber + " "+ numero.length()+" != "+phoneNumber.length(),Toast.LENGTH_SHORT).show();
             }
         }
     }
+    private void sendSMS (String numero, String mensaje,Context context){
+        try {
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(numero,null,mensaje,null,null);
+            Toast.makeText(context, "Mensaje Enviado.", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(context, "Mensaje no enviado, datos incorrectos.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+    public String limpiarNumero (String c){
+        String result="";
+        String t;
+        for(int i=0; i<c.length(); i++){
+            t=c.charAt(i)+"";
+            if(t.equals("+")){
+                c=c.substring(2);
+            }else if(t.matches("[0-9]")){
+                result+=t;
+            }
+        }
+        return result;
+    }
+}
+
+
+
+
 
